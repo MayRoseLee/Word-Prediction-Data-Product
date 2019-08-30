7860dc5ccf2099df9ec4cb5d3721c2b72f7f3b37
May Robinson <31292629+MayRoseLee@users.noreply.github.com>
2019-08-29 21:58:08 -0400

    Add files via upload
    An action button was added to the Shiny app file.  Action button used to control reactivity.

---
+++ app.R
@@ -0,0 +1,28 @@
+# Load data ----
+
+
+source("helpers.R")
+dat <- readRDS("data/dictionary.rds")
+library(stringr) 
+library(dplyr)
+library(shiny)
+ui <- fluidPage(
+  headerPanel("What Will They Say Next?"),
+  textInput(inputId="predictor_words", label = "type in four words(comma separated)",
+  actionButton ("predictButton","predict"),          
+  textOutput(outputId="predicted_word")
+))
+
+server <- function(input, output){
+  
+  myWords = eventReactive(input$predictButton,{
+    unlist(strsplit(input$predictor_words, "\\s*,\\s*"))
+  })
+
+  output$predicted_word <- renderText({
+    paste("", whatsnexta(phrase = input$myWords()))
+  })
+  
+  
+}
+shinyApp (ui=ui, server=server)
